0de59dd4e646dccaf279ddd3caa4a74b4885c84d
Divyam Tiwari <tiwarixdivyam@gmail.com>
2026-01-08 02:03:16 +0530

    project setup , product api setup, and context api setup

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-slate-900 h-screen text-white text-9xl font-mono'>App</div>
+  )
+}
+
+export default App
---
+++ src/context/product/useProduct.js
@@ -0,0 +1,10 @@
+import { useContext } from "react";
+import ProductContext from "./product.context";
+
+const useProduct = () => {
+  const ctx = useContext(ProductContext);
+  if (!ctx) {
+    throw new Error("useProduct must be used within a ProductProvider");
+  }
+  return ctx;
+};
